f2504f2354039801fb821d7bdd7a75f74cce77c6
tkp1n <ndportmann@gmail.com>
2019-02-16 13:40:56 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6a81d075fa83b2108233.js"
+    "url": "webpack-runtime-3708ac27c3d82bb9598d.js"
   },
   {
     "url": "app.8b1cc9920e75fff049f7.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "43135b796fa1f5ad0ee1ecbc78f12bdd"
+    "revision": "a79e217dd3a421d3ece60c129630fb1a"
   },
   {
     "url": "component---src-pages-404-js-14d22c07ddf79bc0cef9.js"
