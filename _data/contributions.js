c14221af5bff0d35aa4a79ee83b4bf32536cdbab
Nicolas Portmann <ndportmann@gmail.com>
2022-10-30 20:07:59 +0100

    Update packages and cleanup

--- _data/contributions.js
+++ _data/contributions.js
@@ -30,21 +30,25 @@ module.exports = function () {
             prUrl: 'https://github.com/SixLabors/ImageSharp/pull/1411',
             prTitle: 'Vectorize (AVX2) JPEG Color Converter'
         },
+        /*
         {
             repoImg: 'https://avatars.githubusercontent.com/u/9141961?s=20&v=4',
             prUrl: 'https://github.com/dotnet/efcore/pull/23014',
             prTitle: 'Avoid lambda capture of cancellation token'
         },
+        */
         {
             repoImg: 'https://avatars.githubusercontent.com/u/9141961?s=20&v=4',
             prUrl: 'https://github.com/dotnet/runtime/pull/37546',
             prTitle: 'Add methods to convert between hexadecimal strings and bytes'
         },
+        /*
         {
             repoImg: 'https://avatars.githubusercontent.com/u/9141961?s=20&v=4',
             prUrl: 'https://github.com/dotnet/corefx/pull/34386',
             prTitle: 'Utf8JsonReader should store if parsed number contains exponent'
         }
+        */
     ];
 
     return source.map(x => ({
